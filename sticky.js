13b0d2e839a5993601a0ed5430fc5e40b86b6858
Scott Sanders <scottsanders87@gmail.com>
2014-07-26 17:43:01 +1000

    Initial plugin commit

---
+++ sticky.js
@@ -0,0 +1,44 @@
+(function($) {
+
+    "use strict";
+
+    $.fn.sticky = function(options) {
+
+        var defaults = {}
+
+        var $this = this,
+            settings = $.extend(defaults, options);
+
+        var init = function($element) {
+
+            var $target = $($element.data("scroll-with"));
+                
+            $(window).scroll(function(){
+                
+                var stickyTop = $target.offset().top - ($target.outerHeight(true) - $target.height()),
+                    stickyBottom = stickyTop + $target.height() - $element.height(),
+                    windowTop = $(this).scrollTop();
+                
+
+                if (windowTop > stickyBottom) {
+                    $element.removeClass('js-sticky').addClass('js-stuck');
+                } else if (stickyTop < windowTop) {
+                    $element.removeClass('js-stuck').addClass('js-sticky');
+                } else {
+                    $element.removeClass('js-sticky').removeClass('js-stuck');
+                }
+
+            });
+
+        };
+
+
+        return this.each(function() {
+
+            init($(this));
+
+        });
+
+    };
+
+}(jQuery));
